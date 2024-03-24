9aae7c6fe12ec92a068f6caead8c8fb697788626
Henrik Binggl <henrik.binggl@gmail.com>
2024-03-24 15:55:11 +0100

    Remove old nvim config

--- .config/nvim/lua/user/nvimtree.lua
+++
@@ -1,3 +0,0 @@
-require'nvim-tree'.setup {}
-
-vim.api.nvim_set_keymap('n', '<F3>', '<cmd>NvimTreeToggle<CR>', { noremap = true, silent=true})
--- .config/nvim/lua/user/uicustom.lua
+++
@@ -1,8 +0,0 @@
--- colorscheme
-vim.g.vscode_style = "dark"
-vim.g.vscode_transparent = 1
-vim.cmd[[colorscheme vscode]]
-
--- statusbar
-require'lualine'.setup()
-
